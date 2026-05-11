11d2a99c45e6493ce394acf0ad4520952f538bbb
Benjamin Gonzalez <ba.gonzalezpa@duocuc.cl>
2026-05-10 21:40:42 -0400

    Se agrega el microservicio Logistica

---
+++ service-logistica/src/main/java/com/donutin/service_logistica/controller/DespachoController.java
@@ -0,0 +1,36 @@
+package com.donutin.service_logistica.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.donutin.service_logistica.model.Despacho;
+import com.donutin.service_logistica.service.DespachoService;
+
+@RestController
+@RequestMapping("/api/v1/despachos")
+public class DespachoController {
+    @Autowired
+    private DespachoService despachoService;
+
+    @PostMapping
+    public Despacho crear(@RequestBody Despacho despacho){
+        return despachoService.guardarDespacho(despacho);
+    }
+
+    @GetMapping("/{id}")
+    public Despacho verDespacho(@PathVariable Long id){
+        return despachoService.obtenerDespacho(id);
+    }
+
+    @GetMapping
+    public List<Despacho> listarDespachos(){
+        return despachoService.listarDespachos();
+    }
+}
