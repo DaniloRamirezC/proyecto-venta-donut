07636d7c16e7aecf8d822c588adb25716a834ea8
DaniloRamirezC <danil.ramirezc@duocuc.cl>
2026-06-07 00:51:56 -0400

    Añadir anotación CrossOrigin en el controller CategoriaController para habilitar CORS

--- service-catalogo/src/main/java/com/donutin/service_catalogo/controller/CategoriaController.java
+++ service-catalogo/src/main/java/com/donutin/service_catalogo/controller/CategoriaController.java
@@ -3,6 +3,7 @@ package com.donutin.service_catalogo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.donutin.service_catalogo.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/api/v1/tipos")
+@CrossOrigin(origins = "*")
 public class CategoriaController 
 {
     @Autowired
