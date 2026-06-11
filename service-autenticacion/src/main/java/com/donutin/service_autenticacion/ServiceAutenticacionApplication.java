713e8d279c6a5d87fd6d38342532c62234e7098e
DaniloRamirezC <danil.ramirezc@duocuc.cl>
2026-06-11 09:23:25 -0400

    Se crea el microservicio service-autenticacion

---
+++ service-autenticacion/src/main/java/com/donutin/service_autenticacion/ServiceAutenticacionApplication.java
@@ -0,0 +1,13 @@
+package com.donutin.service_autenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAutenticacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAutenticacionApplication.class, args);
+	}
+
+}
---
+++ service-autenticacion/src/test/java/com/donutin/service_autenticacion/ServiceAutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donutin.service_autenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
