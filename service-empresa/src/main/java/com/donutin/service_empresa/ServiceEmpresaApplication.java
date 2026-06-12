266771b5dc634cf9ca790e5e92a5aa28dd9525ef
DaniloRamirezC <danil.ramirezc@duocuc.cl>
2026-06-11 23:17:53 -0400

    Se crea microservicio service-empresa

---
+++ service-empresa/src/main/java/com/donutin/service_empresa/ServiceEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.donutin.service_empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEmpresaApplication.class, args);
+	}
+
+}
---
+++ service-empresa/src/test/java/com/donutin/service_empresa/ServiceEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donutin.service_empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
