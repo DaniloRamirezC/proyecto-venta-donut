1643044331c4946b0fe64aad9cdcd99f828a6a08
DaniloRamirezC <danil.ramirezc@duocuc.cl>
2026-06-21 00:44:09 -0400

    Cambio de nombre de variable de SecurityFilterChain

--- service-autenticacion/src/main/java/com/donutin/service_autenticacion/config/SecurityConfig.java
+++ service-autenticacion/src/main/java/com/donutin/service_autenticacion/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig 
 {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         return http
             .csrf(csrf -> csrf.disable())
