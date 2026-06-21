db437d50e03c6c6a2d0807b957f76a368b691707
DaniloRamirezC <danil.ramirezc@duocuc.cl>
2026-06-21 17:22:16 -0400

    Se agrega anotación CrossOrigin en Controller de service-autenticacion

--- service-autenticacion/src/main/java/com/donutin/service_autenticacion/controller/AutenticacionController.java
+++ service-autenticacion/src/main/java/com/donutin/service_autenticacion/controller/AutenticacionController.java
@@ -2,6 +2,7 @@ package com.donutin.service_autenticacion.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "*")
 @Tag(name = "Autenticacion", description = "Endpoints para registro e inicio de sesión de usuarios")
 public class AutenticacionController 
 {
