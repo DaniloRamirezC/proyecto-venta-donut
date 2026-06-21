d5637753c2658e87af4befe257993e9c38742fdb
DaniloRamirezC <danil.ramirezc@duocuc.cl>
2026-06-20 23:29:50 -0400

    Correccion de RequestMapping de controller de service-autenticacion para permitir mapeo de autenticador

--- service-autenticacion/src/main/java/com/donutin/service_autenticacion/controller/AutenticacionController.java
+++ service-autenticacion/src/main/java/com/donutin/service_autenticacion/controller/AutenticacionController.java
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 @Tag(name = "Autenticacion", description = "Endpoints para registro e inicio de sesión de usuarios")
 public class AutenticacionController 
 {
